79a5abac5c46a8c222b580e8ede002e8c8572b28
Ethan Widen <ethan.widen@faulkner.edu>
2018-02-20 21:08:28 +0000

    welcome controller

--- ATS/db/schema.rb
+++ ATS/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220033216) do
+ActiveRecord::Schema.define(version: 20180220205518) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
