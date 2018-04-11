d2de4b1fe545a924e3d118a7cd9ae8a6e84ce7c6
Javan Rampersad <Javan.Rampersad@faulkner.edu>
2018-04-11 21:32:44 +0000

    fleshed out add grad school and add employments

--- ATS/app/controllers/students_controller.rb
+++ ATS/app/controllers/students_controller.rb
@@ -32,6 +32,23 @@ def earned_degree_params
         :degree_id)
 end
 
+def employment_params
+      params.require(:employment).permit(
+        :job_title,
+        :start_date,
+        :current_job,
+        :in_field)
+end
+
+def grad_schools_params
+      params.require(:grad_schools).permit(
+        :applied,
+        :accepted,
+        :status,
+        :higher_degree_type,
+        :higher_degree_name)
+end
+
 def index
     #byebug
     @students = Student.students_in_department(session[:role_id])
@@ -91,7 +108,9 @@ end
   end
     
   def submit_add_grad_school
-    
+    @id = params[:id]
+    @grad_schools = GradSchool.create!(grad_schools_params.merge({student_id: @id}))
+    redirect_to student_path(@id)
   end
   
   def add_employer
@@ -101,7 +120,10 @@ end
   end
   
   def submit_add_employer
-    
+    @id = params[:id]
+    @student = Student.find(@id) 
+    @employment = Employment.create!(employment_params.merge({student_id: @id}))
+    redirect_to student_path(@student)
   end
 
 end
