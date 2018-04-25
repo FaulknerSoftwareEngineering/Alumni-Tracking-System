8ebacc488c45df0a514e00aeb8e4219f59d4206e
Ethan Widen <ethan.widen@faulkner.edu>
2018-04-25 18:10:11 +0000

    #112 implemented security on controllers

--- ATS/app/controllers/application_controller.rb
+++ ATS/app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
-    # before_action :require_login
+    before_action :require_login
   
     def require_login
         if session[:user_id].nil?
