aff1743651103f84e76544de192d20a0db0f64b3
Helio CESAR <mateusc.eng@gmail.com>
2019-04-27 02:44:04 +0000

    teste

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,18 +1,18 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.raise_delivery_errors = true
-
   config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV['GMAIL_SMTP_USER'],
-    user_name: ENV['GMAIL_SMTP_USER'],
-    password: ENV['GMAIL_SMTP_PASSWORD'],
-    authentication: 'plain',
-    enable_starttls_auto: true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings =
+  {
+    :address            => 'smtp-relay.gmail.com',
+    :port               => 587,
+    :domain             => 'gmail.com',
+    :authentication     => :plain,
+    :user_name          => ENV['GMAIL_SMTP_USER'],
+    :password           => ENV['GMAIL_SMTP_PASSWORD'],
+    :enable_starttls_auto => true
   }
 
   config.action_mailer.default_url_options = { :host => 'https://faulkner-ats.herokuapp.com/' }
