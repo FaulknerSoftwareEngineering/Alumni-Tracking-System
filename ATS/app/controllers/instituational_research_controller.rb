81b55806d507e7609094a634592deb24a7ba3f5f
Oceanwalker10 <czlianlap@gmail.com>
2019-05-15 09:39:46 -0500

    save status

--- ATS/app/controllers/instituational_research_controller.rb
+++ ATS/app/controllers/instituational_research_controller.rb
@@ -1,11 +1,10 @@
 class InstituationalResearchController < ApplicationController
-    # Ian Chen 4/8/2019
-    def index
-    end
-    
-    def user
-        @user = User.all
-    end
-
-    
+	# Ian Chen 4/8/2019
+	def index
+		@state = Address.group(:state).count
+	end
+	def user
+		@user = User.all
+	end
+   
 end
